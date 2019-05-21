6b7d0682433678bb315d09f2afbe41160ce8d97a
danlombard <50814298+danlombard@users.noreply.github.com>
2019-05-21 22:30:58 +1000

    Add files via upload

---
+++ polynomial_regression.py
@@ -0,0 +1,58 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_Grid = np.arange(min(X), max(X), 0.1)
+X_Grid = X_Grid.reshape((len(X_Grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_Grid, lin_reg_2.predict(poly_reg.fit_transform(X_Grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with Linear Regression
+y_pred = lin_reg.predict([[6.5]])
+
+# Predicting a new result with Polynomial Regression
+Y_pred1 = lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+Y_pred2 = lin_reg_2.predict(poly_reg.fit_transform([[7.2]]))
+Y_pred3 = lin_reg_2.predict(poly_reg.fit_transform([[4.8]]))
